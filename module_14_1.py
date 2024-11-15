4b00762e748ad65fc52b912a9db28ee0a2497f98
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-11-15 17:48:14 +0400

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+# for i in range(1, 11):
+#     cursor.execute('INSERT INTO Users (username, email, age, balance) '
+#                    'VALUES (?, ?, ?, ?)', (f'User{i}',
+#                    f'example{i}.gmail.com', i * 10, 1000))
+
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500,
+                                                                   f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}',))
+
+cursor.execute('SELECT username, email, age, balance age FROM Users WHERE age '
+               '!= ?', (60,))
+users = cursor.fetchall()
+for i in users:
+    print(f'Имя: {i[0]}, Почта:{i[1]}, Возраст: {i[2]}, баланс{i[3]}')
+
+
+
+
+connection.commit()
+connection.close()
